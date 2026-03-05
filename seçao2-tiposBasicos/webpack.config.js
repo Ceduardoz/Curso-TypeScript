303bf08a2564474b2f1178f65609bd061027387e
ceduardoz <ceduardoz957@gmail.com>
2026-03-05 00:37:27 -0300

    feat: setup webpack configuration for frontend and backend development studies

---
+++ seçao2-tiposBasicos/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+/** @type {import("webpack").Configuration} */
+module.exports = {
+  mode: 'development',
+  entry: './src/aula287-Webpack/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.frontend.json',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
+  },
+  devtool: 'source-map',
+};
